128d9195aaba30d86bcd69bff134e2d8ab48af42
Dericus Horner He/Him <123376826+Dhorner4@users.noreply.github.com>
2023-04-04 20:54:14 -0400

    Create Create-Assign-User-To-OU.ps1

---
+++ Create-Assign-User-To-OU.ps1
@@ -0,0 +1,41 @@
+# Define variables for the AD domain, company name, and org chart
+$domainName = "sunflow.local"
+$companyName = "SunFlow"
+$orgChart = @{
+    "CEO" = "Adam Johnson"
+    "CTO" = "Aisha Khan"
+    "Hardware Engineer" = "Javier Rodriguez"
+    "Software Engineer" = "Amira Ahmed"
+    "Mobile Developer" = "Adam Ali"
+    "Electrical Engineer" = "Maria Garcia"
+    "Mechanical Engineer" = "Ahmed Ali"
+    "Senior Hardware Engineer" = "Tom Wright"
+    "Senior Software Engineer" = "Fatima Hassan"
+    "CFO" = "Sarah Patel"
+    "Director of Operations" = "Mustafa Khan"
+    "Manufacturing Manager" = "Emma Brown"
+    "Procurement Manager" = "Naveed Ahmed"
+    "Logistics Coordinator" = "Abdul Rahman"
+    "Production Coordinator" = "Isabel Lopez"
+    "Director of Sales & Marketing" = "Harry Thompson"
+    "Marketing Manager" = "Sofia Gomez"
+    "Sales Manager" = "Mohammed Ali"
+    "Business Development Manager" = "Mira Patel"
+    "Sales Representative 1" = "Ali Khan"
+    "Sales Representative 2" = "Carmen Rodriguez"
+    "Account Manager" = "Liam Wilson"
+    "Director of Customer Service" = "Olivia Green"
+    "Customer Service Manager" = "Rosa Hernandez"
+    "Technical Support Manager" = "David Chen"
+    "Customer Service Representative 1" = "Georgia Evans"
+    "Customer Service Representative 2" = "Antonio Rodriguez"
+    "Technical Support Representative" = "Aaliyah Ahmed"
+    "Customer Success Manager" = "Tariq Khan"
+}
+# Create the users and assign them to their respective organizational units
+$ouPath = "OU=Engineering,DC=sunflow,DC=local"
+New-ADUser -Name $orgChart["CTO"] -SamAccountName "cto" -UserPrincipalName "cto@$domainName" -Path $ouPath -AccountPassword (ConvertTo-SecureString "P@ssword1" -AsPlainText -Force) -Enabled $true
+$ouPath = "OU=Engineering,DC=sunflow,DC=local"
+New-ADUser -Name $orgChart["Hardware Engineer"] -SamAccountName "hardware" -UserPrincipalName "hardware@$domainName" -Path $ouPath -AccountPassword (ConvertTo-SecureString "P@ssword1" -AsPlainText -Force) -Enabled $true
+$ouPath = "OU=Engineering,DC=sunflow,DC=local"
+New-ADUser -Name $orgChart["Software Engineer"] -SamAccountName "software"-UserPrincipalName "software@$domainName" -Path $ouPath -AccountPassword (ConvertTo-SecureString "P@ssword1" -AsPlainText -Force) -Enabled $true
