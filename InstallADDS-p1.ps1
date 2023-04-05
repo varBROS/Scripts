b7f3939887c579f6708e34353ed82eed41782e64
Marco Aliaga <54997210+kharne8@users.noreply.github.com>
2023-04-05 18:01:14 -0500

    Update and rename InstallADDS.ps1 to InstallADDS-p1.ps1

--- InstallADDS-p1.ps1
+++ InstallADDS-p1.ps1
@@ -1,5 +1,6 @@
 #Installs Active Directory Domain Services(ADDS)
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
 
 #Restart Server
 Restart-Computer -Force
